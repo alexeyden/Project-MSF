20407029397c22f10e746806f7d5806b5edc729c
Alexey Denisov <rtgbnm@gmail.com>
2016-04-08 01:12:18 +0500

    Показ результата в раннере тестов

--- src/tests/run.sh
+++ src/tests/run.sh
@@ -2,6 +2,24 @@
 
 export PYTHONPATH=$PYTHONPATH:`realpath ..`
 
-for x in *.py; do
+failed=()
+
+for x in *_test.py; do
+    echo "Running $x:"
+
     python3 $x -v
+
+    if [[ $? != 0 ]]; then
+        failed[${#failed[*]}]=$x
+    fi
 done
+
+if [[ ${#failed[*]} == 0 ]]; then
+    echo -e "\033[32mAll tests passed\033[0m"
+else
+    echo -e "\033[31m${#failed[*]} test(s) failed:\033[0m"
+
+    for x in ${failed[*]}; do
+        echo "  $x"
+    done
+fi
