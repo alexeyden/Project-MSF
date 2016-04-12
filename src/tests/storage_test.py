f0ec850fd9cc914219cb055bd7cf7b05f28a8091
Alexey Denisov <rtgbnm@gmail.com>
2016-04-13 01:54:23 +0500

    тесты для storage

--- src/tests/storage_test.py
+++ src/tests/storage_test.py
@@ -5,6 +5,8 @@ import os
 
 from server.storage.storage import *
 from server.storage.exceptions import *
+from server.algorithm.type_spec import *
+from server.algorithm.algorithm import *
 
 
 class TestStorage(unittest.TestCase):
@@ -18,7 +20,8 @@ class TestStorage(unittest.TestCase):
         os.mkdir(os.path.join(self.STORAGE, 'user1/A'))
         os.mkdir(os.path.join(self.STORAGE, 'user1/A/B'))
         os.mkdir(os.path.join(self.STORAGE, 'user1/B'))
-        shutil.copy('data/algorithm.json', os.path.join(self.STORAGE, 'user1/A/Alg1'))
+        os.mkdir(os.path.join(self.STORAGE, 'user2/A'))
+        shutil.copy('data/algorithm.json', os.path.join(self.STORAGE, 'user2/A/Alg1'))
 
     def test_init(self):
         self.assertEqual(os.path.exists(os.path.join(self.STORAGE, 'user1')), True)
@@ -53,18 +56,133 @@ class TestStorage(unittest.TestCase):
             self.storage.list('/user4/', context=None)
 
         items = sorted([item.path for item in self.storage.list('/', context=None)])
-
-        self.assertEqual(len(items), 3)
-        self.assertEqual(items[0], '/user1')
-        self.assertEqual(items[1], '/user2')
-        self.assertEqual(items[2], '/user3')
+        self.assertListEqual(items, [
+            '/user1',
+            '/user2',
+            '/user3'
+        ])
 
         context = Storage.StorageContext('user1')
-        items = sorted([item.path for item in self.storage.list('/', context)])
 
-        self.assertEqual(len(items), 2)
-        self.assertEqual(items[0], '/user1')
-        self.assertEqual(items[1], '/user2')
+        items = sorted([item.path for item in self.storage.list('/', context)])
+        self.assertListEqual(items, [
+            '/user1',
+            '/user2'
+        ])
+
+        items = sorted([item.path for item in self.storage.list('/user1/', context)])
+        self.assertListEqual(items, [
+            '/user1/A',
+            '/user1/B'
+        ])
+
+        items = sorted([item.path for item in self.storage.list('/user1/A/', context)])
+        self.assertListEqual(items, [
+            '/user1/A/B'
+        ])
+
+        items = sorted([item.path for item in self.storage.list('/user2/A/', context)])
+        self.assertListEqual(items, [
+            '/user2/A/Alg1'
+        ])
+
+    def test_move(self):
+        context = Storage.StorageContext('user1')
+
+        loop = asyncio.get_event_loop()
+
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.move('% invalid path %', '/user1/C', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.move('/user1/A', '% udsds %', context))
+
+        with self.assertRaises(NoSuchPathError):
+            loop.run_until_complete(self.storage.move('/user1/C/', '/user1/D', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.move('/user1/A/', '/user1/B', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.move('/user1/A/', '/', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.move('/user1/A/', '/user2/C/', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.move('/user2/A/', '/user1/C/', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.move('/', '/user2/C/', context))
+
+        loop.run_until_complete(self.storage.move('/user1/A/', '/user1/B/C', context))
+        self.assertEqual(self.storage.exists('/user1/A', context), False)
+        self.assertEqual(self.storage.exists('/user1/B/C/', context), True)
+
+        loop.run_until_complete(self.storage.move('/user1/B/C', '/user1/A', context))
+        self.assertEqual(self.storage.exists('/user1/A', context), True)
+        self.assertEqual(self.storage.exists('/user1/B/C/', context), False)
+
+    def test_remove(self):
+        context = Storage.StorageContext('user1')
+
+        loop = asyncio.get_event_loop()
+
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.remove('% invalid path %', context))
+        with self.assertRaises(NoSuchPathError):
+            loop.run_until_complete(self.storage.remove('/user1/C', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.remove('/user2/A', context))
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.remove('/user2', context))
+
+        os.mkdir(os.path.join(self.STORAGE, 'user1/C'))
+        loop.run_until_complete(self.storage.remove('/user1/C', context))
+        self.assertEqual(os.path.exists(os.path.join(self.STORAGE, 'user1/C')), False)
+
+    def test_file_read(self):
+        context = Storage.StorageContext('user1')
+        loop = asyncio.get_event_loop()
+
+        with self.assertRaises(InvalidPathError):
+            loop.run_until_complete(self.storage.file_read('%%invalid', context))
+        with self.assertRaises(NoSuchPathError):
+            loop.run_until_complete(self.storage.file_read('/user1/С', context))
+        with self.assertRaises(NoSuchPathError):
+            loop.run_until_complete(self.storage.file_read('/user2/A/'))
+
+        a = loop.run_until_complete(self.storage.file_read('/user2/A/Alg1'))
+        self.assertEqual(a.source, '(some (source (here)))')
+
+    def test_create(self):
+        context = Storage.StorageContext('user1')
+        loop = asyncio.get_event_loop()
+
+        loop.run_until_complete(self.storage.create('/user1/A/С', context=context))
+        self.assertEqual(self.storage.exists('/user1/A/С', context=context), True)
+        loop.run_until_complete(self.storage.remove('/user1/A/С', context=context))
+
+        a = Algorithm(input_spec=[TypeSpec(type_=TypeSpec.INT)], output_spec=TypeSpec(type_=TypeSpec.INT), source='(foo)')
+        loop.run_until_complete(self.storage.create('/user1/A/File', context=context, content=a))
+        self.assertEqual(self.storage.exists('/user1/A/File', context=context), True)
+        a = loop.run_until_complete(self.storage.file_read('/user1/A/File'))
+        self.assertEqual(a.source, '(foo)')
+        loop.run_until_complete(self.storage.remove('/user1/A/File', context=context))
+
+    def test_file_write(self):
+        context = Storage.StorageContext('user1')
+        loop = asyncio.get_event_loop()
+
+        with self.assertRaises(NoSuchPathError):
+            loop.run_until_complete(self.storage.file_read('/user1/A', context))
+        with self.assertRaises(NoSuchPathError):
+            loop.run_until_complete(self.storage.file_read('/user1/D', context))
+
+        a = Algorithm(input_spec=[TypeSpec(type_=TypeSpec.INT)], output_spec=TypeSpec(type_=TypeSpec.INT), source='(foo)')
+        loop.run_until_complete(self.storage.create('/user1/A/File', context=context, content=a))
+
+        a.source = '(bar)'
+        loop.run_until_complete(self.storage.file_write('/user1/A/File', context=context, content=a))
+
+        b = loop.run_until_complete(self.storage.file_read('/user1/A/File'))
+        self.assertEqual(b.source, '(bar)')
+
+        loop.run_until_complete(self.storage.remove('/user1/A/File', context=context))
 
     def test_exists(self):
         self.assertEqual(self.storage.exists('/'), True)
