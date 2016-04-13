30415b02988707a461b8de31e8606d5bae3abce8
Alexey Denisov <rtgbnm@gmail.com>
2016-04-13 14:06:36 +0500

    тесты

--- src/tests/user_access_test.py
+++ src/tests/user_access_test.py
@@ -7,11 +7,18 @@ from storage.exceptions import ConfigError
 
 
 class TestUserAccess(unittest.TestCase):
+    def test_auth(self):
+        users = UserAccess(storage=None, config_path='data/users_1.json')
+        self.assertIsNotNone(users.auth('user1', '123'))
+        self.assertIsNotNone(users.auth('user2', '456'))
+        self.assertIsNone(users.auth('user1', 'invalid'))
+        self.assertIsNone(users.auth('user100', 'invalid'))
+
     def test_query(self):
         users = UserAccess(storage=None, config_path='data/users_1.json')
 
-        u1 = (users.by_login("user1"), User(login="user1", password="123"))
-        u2 = (users.by_login("user2"), User(login="user2", password="456"))
+        u1 = (users.by_login("user1"), User(login="user1", password="202cb962ac59075b964b07152d234b70"))
+        u2 = (users.by_login("user2"), User(login="user2", password="250cf8b51c773f3f8dc8b4be867a9a02"))
 
         for uc,ut in [u1, u2]:
             self.assertEqual(uc, ut)
@@ -21,6 +28,15 @@ class TestUserAccess(unittest.TestCase):
             UserAccess(None, 'data/not_existing.json')
         with self.assertRaises(ConfigError):
             UserAccess(None, 'data/users_wrong.json')
+
+    def test_create_remove(self):
+        users = UserAccess(storage=None, config_path='data/users_1.json')
+        self.assertEqual(users.create('test', 'test'), True)
+        self.assertEqual('test' in  users.users, True)
+        self.assertEqual(users.change_password('test', 'test2'), True)
+        self.assertEqual(users.users['test'].password, users._hash_password('test2'))
+        self.assertEqual(users.remove('test'), True)
+        self.assertEqual('test' in users.users, False)
 
 if __name__ == '__main__':
     unittest.main()
