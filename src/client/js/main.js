066caaa646fde3a220847399476f5e1de341102e
MrAmiD <Nadymov2910@gmail.com>
2016-04-14 15:27:42 +0500

    Добавил клиент

---
+++ src/client/js/main.js
@@ -0,0 +1,29 @@
+
+  $(function() {
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#find" ).autocomplete({
+      source: availableTags
+    });
+  });
